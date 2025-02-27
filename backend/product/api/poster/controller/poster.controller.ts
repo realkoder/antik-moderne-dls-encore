8704567b0098accdcf41f215ae4e64ece97ec30a
realkoder <alexanderbtcc@gmail.com>
2025-02-27 23:17:57 +0100

    add: createPoster POST endoint

--- backend/product/api/poster/controller/poster.controller.ts
+++ backend/product/api/poster/controller/poster.controller.ts
@@ -1,15 +1,22 @@
 import { api, APIError } from "encore.dev/api";
-import { PosterDto } from "../../../types/poster.interface";
+import { PosterCreate, PosterDto } from "../../../types/poster.interface";
 import PosterService from "../service/poster.service";
-
-
-
 
 export const getPosters = api<{}, { posters: PosterDto[] }>(
     { auth: false, expose: true, method: "GET", path: "/posters" },
     async (): Promise<{ posters: PosterDto[] }> => {
         const posters = await PosterService.findAll();
-        
+
+        return { posters };
+    }
+);
+
+export const createPoster = api<{ posterCreate: PosterCreate }, { posters: PosterDto[] }>(
+    { auth: false, expose: true, method: "POST", path: "/posters" },
+    async ({ posterCreate }: { posterCreate: PosterCreate }): Promise<{ posters: PosterDto[] }> => {
+        await PosterService.create(posterCreate);
+        const posters = await PosterService.findAll();
+
         return { posters };
     }
 );

b1291572c22ed7859fec114bdc22ea5859b64f1f
realkoder <alexanderbtcc@gmail.com>
2025-03-12 15:47:57 +0100

    fixed issue with naming org service

--- backend/organization/db/database.ts
+++ backend/organization/db/database.ts
@@ -1,19 +1,19 @@
-// import { SQLDatabase } from "encore.dev/storage/sqldb";
-// import { PrismaClient } from "@prisma-db-organizations/client";
+import { SQLDatabase } from "encore.dev/storage/sqldb";
+import { PrismaClient } from "@prisma-db-organizations/client";
 
-// const DB = new SQLDatabase("organizations", {
-//   migrations: {
-//     path: "migrations",
-//     source: "prisma",
-//   },
-// });
+const DB = new SQLDatabase("organizations", {
+  migrations: {
+    path: "migrations",
+    source: "prisma",
+  },
+});
 
-// const prismaOrganizations = new PrismaClient({
-//   datasources: {
-//     db: {
-//       url: DB.connectionString,
-//     },
-//   },
-// });
+const prismaOrganizations = new PrismaClient({
+  datasources: {
+    db: {
+      url: DB.connectionString,
+    },
+  },
+});
 
-// export { prismaOrganizations };
+export { prismaOrganizations };
--- backend/organization/encore.service.ts
+++ backend/organization/encore.service.ts
@@ -1,3 +1,3 @@
 import { Service } from "encore.dev/service";
 
-export default new Service("user");
+export default new Service("organization");

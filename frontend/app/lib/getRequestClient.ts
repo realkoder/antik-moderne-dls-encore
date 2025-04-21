068a2881ef9ce28e441e654399a41c4a164b18a5
realkoder <alexanderbtcc@gmail.com>
2025-04-21 21:26:19 +0200

    Added logs to check for correct env vars

--- frontend/app/lib/getRequestClient.ts
+++ frontend/app/lib/getRequestClient.ts
@@ -6,10 +6,10 @@ import Client, { Environment, Local } from "./client";
  * If we are running the frontend locally we assume that our Encore backend is also running locally.
  */
 const getRequestClient = (token: string | undefined) => {
-  const env = import.meta.env.DEV ? Local : import.meta.env.NODE_ENV === "local-kubernetes" ? Local : Environment("staging");
+  const env = import.meta.env.DEV ? Local : import.meta.env.VITE_ENV === "local-kubernetes" ? Local : Environment("staging");
 
 console.log("LOOOK META FULL ENV", import.meta.env)  
-console.log("LOOOK IMPORTANT NODE_ENV", import.meta.env.NODE_ENV)  
+console.log("LOOOK IMPORTANT NODE_ENV", import.meta.env.VITE_ENV)
 
   return new Client(env, {
     auth: { authorization: token || "" },

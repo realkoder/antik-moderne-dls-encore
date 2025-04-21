6424390caa03db907ab5426fec7947631fbcc663
realkoder <alexanderbtcc@gmail.com>
2025-04-21 21:47:30 +0200

    relying on if production for now going wiuth LOCAL route since kuberntes localhosted

--- frontend/app/lib/getRequestClient.ts
+++ frontend/app/lib/getRequestClient.ts
@@ -6,10 +6,7 @@ import Client, { Environment, Local } from "./client";
  * If we are running the frontend locally we assume that our Encore backend is also running locally.
  */
 const getRequestClient = (token: string | undefined) => {
-  const env = import.meta.env.DEV ? Local : import.meta.env.VITE_ENV === "local-kubernetes" ? Local : Environment("staging");
-
-console.log("LOOOK META FULL ENV", import.meta.env)  
-console.log("LOOOK IMPORTANT NODE_ENV", import.meta.env.VITE_ENV)
+  const env = import.meta.env.DEV ? Local : import.meta.env.MODE === "production" ? Local : Environment("staging");
 
   return new Client(env, {
     auth: { authorization: token || "" },
